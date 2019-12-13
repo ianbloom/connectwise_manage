bc153725ff6e2d06580e343634c475a1c44ca6ca
Stuart Weenig <stuart@weenig.com>
2019-12-13 16:17:06 -0600

    Fixed cw_get functions. cw_post/patch are still not working. Something missing from the config data being posted.

--- api_helpers/cw_api.py
+++ api_helpers/cw_api.py
@@ -1,6 +1,4 @@
 #! /usr/bin/python
-
-# from api_func import *
 import json
 import base64
 import requests
@@ -8,7 +6,6 @@ import requests
 ###########
 # HELPERS #
 ###########
-
 def header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId):
 	header_dict = {}
 	header_dict['Content-Type'] = 'application/json'
@@ -21,75 +18,61 @@ def header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId):
 ###########
 # GETTERS #
 ###########
-#all of these functions need to be modified so that the body returned is a python list of objects, ready to use without further decode/json.loads needed.
 def get_cw_config_list(_cw_api_id, _cw_api_key, _cw_company, _cw_site, _cw_agentId):
 	url = f'https://{_cw_site}/v4_6_release/apis/3.0/company/configurations'
-	header_dict = header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId)
-	data = ''
-	response = requests.get(url, data=data, headers=header_dict)
-	return {'code':response.status_code, 'items':response.content}
+	response = requests.get(url, data="", headers=header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId))
+	devices = {}
+	if response.status_code in (200, 201):
+		for device in json.loads(response.content): devices[device['name']] = device['id']
+	else:
+		devices = response.content
+	return {'code':response.status_code, 'items':devices}
 
 def get_cw_company_list(_cw_api_id, _cw_api_key, _cw_company, _cw_site, _cw_agentId):
-	# Needs to return a list of company IDs from CW
 	url = f'https://{_cw_site}/v4_6_release/apis/3.0/company/companies'
-	header_dict = header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId)
-	data = ''
-	response = requests.get(url, data=data, headers=header_dict)
-	return {'code':response.status_code, 'items':response.content}
+	response = requests.get(url, data="", headers=header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId))
+	companies = {}
+	if response.status_code in (200, 201):
+		for company in json.loads(response.content):
+			companies[company['name']] = {
+				'id': company['id'],
+				'identifier': company['identifier']
+			}
+	else:
+		companies = response.content
+	return {'code':response.status_code, 'items':companies}
 
 def get_cw_type_list(_cw_api_id, _cw_api_key, _cw_company, _cw_site, _cw_agentId):
-	# Needs to return a list of company IDs from CW
 	url = f'https://{_cw_site}/v4_6_release/apis/3.0/company/configurations/types'
-	header_dict = header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId)
-	data = ''
-	response = requests.get(url, data=data, headers=header_dict)
-	return {'code':response.status_code, 'items':response.content}
+	response = requests.get(url, data="", headers=header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId))
+	types = {}
+	if response.status_code in (200, 201):
+		for type in json.loads(response.content): types[type['name']] = type['id']
+	else:
+		types = response.content
+	return {'code':response.status_code, 'items':types}
 
 ###########
 # POSTERS #
 ###########
-
 def post_cw_configuration(_cw_api_id, _cw_api_key, _cw_company, _cw_site, _cw_agentId, _config_dict):
-	# Build request URL
 	url = f'https://{_cw_site}/v4_6_release/apis/3.0/company/configurations'
-
-	# Build header dictionary
-	header_dict = header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId)
-
-	# Data is a json string
 	data = json.dumps(_config_dict)
-
-	response = requests.post(url, data=data, headers=header_dict)
-
-	return_dict = {'code':response.status_code,
-				   'body':response.content}
-
-	return return_dict
+	print(data)
+	response = requests.post(url, data=data, headers=header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId))
+	return {'code':response.status_code, 'body':response.content}
 
 ############
 # PATCHERS #
 ############
-
 def patch_cw_configuration(_cw_api_id, _cw_api_key, _cw_company, _cw_site, _cw_agentId, _config_dict, _config_id):
-	# Build request URL
 	url = f'https://{_cw_site}/v4_6_release/apis/3.0/company/configurations/{_config_id}'
-
-	# Build header dictionary
-	header_dict = header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId)
-
 	patch_array = []
-
 	for key, value in _config_dict.items():
 		patch_dict = {'op':'replace',
 					  'path':key,
 					  'value':value}
 		patch_array.append(patch_dict)
-
 	data = json.dumps(patch_array)
-
-	response = requests.patch(url, data=data, headers=header_dict)
-
-	return_dict = {'code':response.status_code,
-				   'body':response.content}
-
-	return return_dict
+	response = requests.patch(url, data=data, headers=header_build(_cw_company, _cw_api_id, _cw_api_key, _cw_agentId))
+	return {'code':response.status_code, 'body':response.content}
